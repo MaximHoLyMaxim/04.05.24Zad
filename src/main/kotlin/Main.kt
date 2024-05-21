994dbc23a3099a4d20c1e353f3fa23d80d9d8414
Maxim Igumnov <161121113+MaximHoLyMaxim@users.noreply.github.com>
2024-05-21 23:39:29 +0300

    Main.kt

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+class Dragon: Monster() {
+    override fun frighten(): Boolean {
+        println("Fire!")
+        return true
+    }
+}
+
+fun main() {
+    val m = arrayOf(
+        Vampire(),
+        Dragon(),
+        Monster()
+    )
+    for (item in m) {
+        item.frighten()
+    }
+}
+open class Monster {
+    open fun frighten(): Boolean {
+        println("Aargh!")
+        return false
+    }
+}
+class Vampire: Monster() {
+    override fun frighten(): Boolean {
+        println("Fancy a bite?")
+        return true
+    }
+}
